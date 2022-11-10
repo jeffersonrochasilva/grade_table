a1a7bcad747ddef2f34182ea1e4c67b9c9bfcf79
DaviAugustoMS     <jeffersonrochadasilva667@gmail.com>
2022-11-09 23:07:51 -0300

    slad

--- src/pages/private/Dispensa.vue
+++ src/pages/private/Dispensa.vue
@@ -44,6 +44,17 @@
         </div>
       </div>
     </v-card>
+    <template>
+      <v-carousel v-model="model">
+        <v-carousel-item v-for="(color, i) in colors" :key="color">
+          <v-sheet :color="color" height="100%" tile>
+            <v-row class="fill-height" align="center" justify="center">
+              <div class="text-h2">Slide {{ i + 1 }}</div>
+            </v-row>
+          </v-sheet>
+        </v-carousel-item>
+      </v-carousel>
+    </template>
   </div>
 </template>
 
@@ -51,6 +62,8 @@
 export default {
   data: () => ({
     titlecolor: "red",
+    model: 0,
+    colors: ["primary", "secondary", "yellow darken-2", "red", "orange"],
   }),
   computed: {
     descriptionData() {
