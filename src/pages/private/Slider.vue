28b6f3dadb21055b6c2d25185c626955dac1ad32
DaviAugustoMS     <jeffersonrochadasilva667@gmail.com>
2022-11-14 22:36:35 -0300

    ajustes

--- src/pages/private/Slider.vue
+++ src/pages/private/Slider.vue
@@ -8,6 +8,33 @@
       transition="fade-transition"
     ></v-carousel-item>
   </v-carousel>
+  <v-carousel>
+    <v-carousel-item
+      v-for="(item, i) in items"
+      :key="i"
+      :src="item.src"
+      reverse-transition="fade-transition"
+      transition="fade-transition"
+    ></v-carousel-item>
+  </v-carousel>
+  <v-carousel>
+    <v-carousel-item
+      v-for="(item, i) in items"
+      :key="i"
+      :src="item.src"
+      reverse-transition="fade-transition"
+      transition="fade-transition"
+    ></v-carousel-item>
+  </v-carousel>
+  <v-carousel>
+    <v-carousel-item
+      v-for="(item, i) in items"
+      :key="i"
+      :src="item.src"
+      reverse-transition="fade-transition"
+      transition="fade-transition"
+    ></v-carousel-item>
+  </v-carousel>
 </template>
 <script>
 export default {
