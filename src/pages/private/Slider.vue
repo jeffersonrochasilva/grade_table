9e68074dac560b7d6531c40478de4e6969725183
DaviAugustoMS     <jeffersonrochadasilva667@gmail.com>
2022-11-23 00:44:44 -0300

    novas auteracoes

--- src/pages/private/Slider.vue
+++ src/pages/private/Slider.vue
@@ -70,7 +70,8 @@
       reverse-transition="fade-transition"
       transition="fade-transition"
     ></v-carousel-item>
-  </v-carousel>  <v-carousel>
+  </v-carousel>
+  <v-carousel>
     <v-carousel-item
       v-for="(item, i) in items"
       :key="i"
@@ -78,7 +79,8 @@
       reverse-transition="fade-transition"
       transition="fade-transition"
     ></v-carousel-item>
-  </v-carousel>  <v-carousel>
+  </v-carousel>
+  <v-carousel>
     <v-carousel-item
       v-for="(item, i) in items"
       :key="i"
@@ -86,7 +88,8 @@
       reverse-transition="fade-transition"
       transition="fade-transition"
     ></v-carousel-item>
-  </v-carousel>  <v-carousel>
+  </v-carousel>
+  <v-carousel>
     <v-carousel-item
       v-for="(item, i) in items"
       :key="i"
@@ -94,7 +97,44 @@
       reverse-transition="fade-transition"
       transition="fade-transition"
     ></v-carousel-item>
-  </v-carousel>  <v-carousel>
+  </v-carousel>
+  <v-carousel>
+    <v-carousel-item
+      v-for="(item, i) in items"
+      :key="i"
+      :src="item.src"
+      reverse-transition="fade-transition"
+      transition="fade-transition"
+    ></v-carousel-item>
+  </v-carousel>
+  <v-carousel>
+    <v-carousel-item
+      v-for="(item, i) in items"
+      :key="i"
+      :src="item.src"
+      reverse-transition="fade-transition"
+      transition="fade-transition"
+    ></v-carousel-item>
+  </v-carousel>
+  <v-carousel>
+    <v-carousel-item
+      v-for="(item, i) in items"
+      :key="i"
+      :src="item.src"
+      reverse-transition="fade-transition"
+      transition="fade-transition"
+    ></v-carousel-item>
+  </v-carousel>
+  <v-carousel>
+    <v-carousel-item
+      v-for="(item, i) in items"
+      :key="i"
+      :src="item.src"
+      reverse-transition="fade-transition"
+      transition="fade-transition"
+    ></v-carousel-item>
+  </v-carousel>
+  <v-carousel>
     <v-carousel-item
       v-for="(item, i) in items"
       :key="i"
