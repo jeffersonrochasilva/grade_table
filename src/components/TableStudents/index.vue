d8ffe5ecd2608ae3dbee7f41b54e3d726c903226
DaviAugustoMS     <jeffersonrochadasilva667@gmail.com>
2023-02-02 23:19:14 -0300

    ajuste

--- src/components/TableStudents/index.vue
+++ src/components/TableStudents/index.vue
@@ -84,6 +84,69 @@
         <v-btn class="btn" @click="deleteItemList(item.id)">excluir</v-btn>
       </div>
     </div>
+    <div class="table__content" v-for="(item, i) in students" :kye="i">
+      <div class="table__content__checkbox">
+        <v-checkbox @click="getItemList(item)" />
+      </div>
+      <div class="table__content__id">
+        <span>{{ item.id }}</span>
+      </div>
+      <div class="table__content__email">
+        <span>{{ item.email }}</span>
+      </div>
+      <div class="table__content__name">
+        <span>{{ item.nome }}</span>
+      </div>
+
+      <div class="table__content__box">
+        <v-icon class="table__content__box__icon">mdi-clipboard-account</v-icon>
+      </div>
+      <div class="table__content__button">
+        <v-btn class="btn" @click="deleteItemList(item.id)">excluir</v-btn>
+      </div>
+    </div>
+    <div class="table__content" v-for="(item, i) in students" :kye="i">
+      <div class="table__content__checkbox">
+        <v-checkbox @click="getItemList(item)" />
+      </div>
+      <div class="table__content__id">
+        <span>{{ item.id }}</span>
+      </div>
+      <div class="table__content__email">
+        <span>{{ item.email }}</span>
+      </div>
+      <div class="table__content__name">
+        <span>{{ item.nome }}</span>
+      </div>
+
+      <div class="table__content__box">
+        <v-icon class="table__content__box__icon">mdi-clipboard-account</v-icon>
+      </div>
+      <div class="table__content__button">
+        <v-btn class="btn" @click="deleteItemList(item.id)">excluir</v-btn>
+      </div>
+    </div>
+    <div class="table__content" v-for="(item, i) in students" :kye="i">
+      <div class="table__content__checkbox">
+        <v-checkbox @click="getItemList(item)" />
+      </div>
+      <div class="table__content__id">
+        <span>{{ item.id }}</span>
+      </div>
+      <div class="table__content__email">
+        <span>{{ item.email }}</span>
+      </div>
+      <div class="table__content__name">
+        <span>{{ item.nome }}</span>
+      </div>
+
+      <div class="table__content__box">
+        <v-icon class="table__content__box__icon">mdi-clipboard-account</v-icon>
+      </div>
+      <div class="table__content__button">
+        <v-btn class="btn" @click="deleteItemList(item.id)">excluir</v-btn>
+      </div>
+    </div>
   </div>
 </template>
 
